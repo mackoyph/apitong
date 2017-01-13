a65cd50fb993de1aa788c5f28815fcaaf98cc5e3
Sony Mikhael Fampulme <sonymikhael07@gmail.com>
2017-01-14 07:54:02 +0800

    Added logic to display the main About Us article in the admin panel

--- admin2/application/views/about/view.php
+++ admin2/application/views/about/view.php
@@ -20,9 +20,20 @@
 					<a href="#" class='btn btn-primary pull-right'>Edit</a>
 				</div>
 				<div class='box-body'>
-				<h4><b>Title: </b><?php echo $main_about->title; ?></h4>
-				<br/><b>Text:</b><br/>
-				<?php echo html_entity_decode($main_about->text); ?>
+					<?php
+					if (isset($main_about))
+					{
+					?>
+						<h4><b>Title: </b><?php echo $main_about->title; ?></h4>
+						<br/><b>Text:</b><br/>
+						<?php echo html_entity_decode($main_about->text); ?>
+					<?php 
+					} 
+					else
+					{
+						echo "No About Us page yet.";
+					}
+					?>
 				</div>
 			</div> <!-- end box -->
 		</div>
