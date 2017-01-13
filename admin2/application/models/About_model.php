b900899727d4f703c6c2c9b9215eca86e35e1a09
Sony Mikhael Fampulme <sonymikhael07@gmail.com>
2017-01-14 07:51:22 +0800

    Added debug function and messages to About model, corrected the getArticles method

--- admin2/application/models/About_model.php
+++ admin2/application/models/About_model.php
@@ -1,29 +1,43 @@
 <?php
 Class About_model extends CI_Model
 {
-	private $root_category = 1;
+	private $c = "ABOUT MODEL";
+
+	private function debug($function, $message)
+	{
+		log_message('debug', $this->c . " : " . $function . " : " . $message);
+	}
+
+	private $About = 2;
+
 	function getMainAboutArticle()
 	{
 		$this->db->select('*')
 				->from('article')
-				->where('category', $this->root_category);
+				->where('category', $this->About);
 		$query = $this->db->get();
 		return $query -> result();
 	}
 	
-	function getArticles()
+	function getAboutArticles()
 	{
-		$this->db->select('*')
+		$this->db->select('article.*, article_category.name, author.ACCESS_USERNAME AS `author_username`, editor.ACCESS_USERNAME AS `editor_username`')
 				->from('article')
-				->where('category !=', $this->root_category);
+				->join('article_category', 'article.category=article_category.id')
+				->join('admin_access AS `author`', 'article.author_id=author.ACCESS_NO')
+				->join('admin_access AS `editor`', 'article.last_edited_by=editor.ACCESS_NO')
+				->where('root_category', $this->About);
+		//$string = $this->db->get_compiled_select('article',FALSE);
+		//$this->debug('getAboutArticles', 'query='.var_export($string, TRUE));
 		$query = $this->db->get();
 		return $query->result();
 	}
 	
-	function getCategories()
+	function getAboutCategories()
 	{
 		$this->db->select('*')
-				->from('article_category');
+				->from('article_category')
+				->where('root_category', $this->About);
 		$query = $this->db->get();
 		return $query->result();
 	}
