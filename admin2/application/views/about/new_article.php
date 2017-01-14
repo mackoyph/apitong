d4cffd766edd16d066da7ec1a2e8b11f01a4333e
Sony Mikhael Fampulme <sonymikhael07@gmail.com>
2017-01-14 17:20:04 +0800

    new_article view file

---
+++ admin2/application/views/about/new_article.php
@@ -0,0 +1,33 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+	<h1>
+		Apitong Village Website Admin Panel
+		<small>Optional description</small>
+	</h1>
+	<ol class="breadcrumb">
+		<li><a href="<?php echo base_url('home');?>"><i class="fa fa-dashboard"></i> Home</a></li>
+	</ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+	<div class='row'>
+		<div class='col-md-12'>
+			<div class='box box-primary'>
+				<div class='box-header with-border'>
+					<h3 class='box-title'>Compose New Article</h3>
+				</div>
+				<!-- end box header-->
+				<div class='box-body'>
+					<div class='form-group'>
+						<input class='form-control' placeholder='Title'>
+					</div>
+				</div>
+			</div>
+			<!-- end box -->
+		</div>
+		<!-- end col 12 -->
+	</div>
+	<!-- end row -->
+</section>
+<!-- /.content -->
