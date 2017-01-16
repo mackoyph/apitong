918f0921943032d51f016fa7a6c0e8a807f8671a
Sony Mikhael Fampulme <sonymikhael07@gmail.com>
2017-01-16 13:51:33 +0800

    added elements to the listing view file

--- admin2/application/views/announcement/listing.php
+++ admin2/application/views/announcement/listing.php
@@ -37,7 +37,7 @@
 								echo "<td>" . htmlentities($row->title) . '<br/>';
 								echo "<div class='btn-group' role='group'>";
 								echo "<a href='" . base_url('announcement/edit/' . $row->id) . "' class='btn btn-default btn-xs'>Edit</a>";
-								echo "<button data-toggle='modal' data-target='deleteAnnouncementModal' class='btn btn-default btn-xs'>Delete</button></div>";
+								echo "<button data-toggle='modal' data-target='#deleteAnnouncementModal' class='btn btn-default btn-xs' data-title='" . $row->title . "' data-articleid='" . $row->id . "'>Delete</button></div>";
 								echo  "</td>";
 								echo "<td>" . htmlentities(substr($row->text, 0, 100)) . " ... </td>";
 								echo "<td> Author: " . $row->author . "<br/>Last Edited by: " . $row->editor . "<br/>Last edited on: " . $row->last_edit_date . "</td>"; 
@@ -67,6 +67,24 @@
 			<!-- end box -->
 		</div>
 	</div>
+	<div class="modal fade" id='deleteAnnouncementModal' tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title">Delete Announcement Confirmation</h4>
+	  			</div>
+	  			<div class="modal-body">
+					<p>Are you sure you want to delete the Announcement ?</p>
+					<p id='title-to-delete'></p>
+	  			</div>
+	  			<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<a id='deleteArticleLink' class="btn btn-warning">Delete</a>
+	  			</div>
+			</div><!-- /.modal-content -->
+  		</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
 </section>
 <!-- /.content -->
 
@@ -79,6 +97,15 @@ $(function(){
 		"paging":false,
 		"searching":false,
 		"info":false
+	});
+	$('#deleteAnnouncementModal').on('show.bs.modal', function (event) {
+		var button = $(event.relatedTarget) // Button that triggered the modal
+		var articleid = button.data('articleid');
+		var title = button.data('title');
+		$('#title-to-delete').text("Title: " + title);
+		console.log("article id=" + articleid);
+		$("#deleteArticleLink").attr('href', "<?php echo base_url('announcement/delete/');?>" + articleid);
+
 	});
 });
 </script>
