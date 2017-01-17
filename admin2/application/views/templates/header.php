cd2cff8eb7fe63e8a70396cbbc4ddcf19b32957e
Sony Mikhael Fampulme <sonymikhael07@gmail.com>
2017-01-17 21:47:37 +0800

    removed datepicker css which is not used

--- admin2/application/views/templates/header.php
+++ admin2/application/views/templates/header.php
@@ -23,8 +23,6 @@
 
 	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/select2/select2.min.css');?>">
 
-	<link rel="stylesheet" href="<?php echo base_url('assets/plugins/datepicker/datepicker3.css');?>">
-
 	<!-- Theme style -->
 	<link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.min.css');?>">
 	
